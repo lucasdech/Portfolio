2395d9e03eeb214a30ea9f6f497a9560c68d2f6c
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-05-14 16:39:25 +0200

    test formulaire

--- contact.php
+++ contact.php
@@ -3,6 +3,7 @@
 try {
     //code...
     mail("lucasdechavanne22@gmail.com", $_POST['firstname'] . ' '. $_POST['lastname'], $_POST['message']);
+    var_dump('done');
 } catch (\Throwable $th) {
     throw $th;
 }
