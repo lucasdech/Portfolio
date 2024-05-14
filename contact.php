e16863eba65e3ebcf3f2cad9a042e706200d1827
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-05-14 16:46:09 +0200

    test formulaire

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$headers = 'From:'. $_POST['mail'] . "\r\n" .
+$headers = 'From:<'. $_POST['mail'] . ">\r\n" .
 
 mail('<lucasdechavanne22@gmail.com>', 'the subject', 'the message', $headers,
   '-fwebmaster@example.com');
