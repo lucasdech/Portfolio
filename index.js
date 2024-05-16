86d9559a0cd2574a06e16ce5abfb0b59ad157e80
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-05-16 21:00:24 +0200

    premiere mise en ligne du portfolio

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ let menu_burger = document.querySelector('#menu_burger')
 
 menu_burger_icon.addEventListener('click',function() {
 
+    menu_burger.classList.toggle('background_burguer')
     menu_burger_items.classList.toggle('hidden_div')
     menu_burger_items.classList.add('menu_burger_bg')
 })
+
+let burger_status = document.querySelector('#burger_status')
+let burger_presentation = document.querySelector('#burger_presentation')
+let burger_projets = document.querySelector('#burger_projets')
+
+
+burger_status.addEventListener('click', function () {
+    menu_burger.classList.toggle('background_burguer')
+    menu_burger_items.classList.toggle('hidden_div')
+    
+})
+
+burger_presentation.addEventListener('click', function () {
+    menu_burger.classList.toggle('background_burguer')
+    menu_burger_items.classList.toggle('hidden_div')
+    
+})
+burger_projets.addEventListener('click', function () {
+    menu_burger.classList.toggle('background_burguer')
+    menu_burger_items.classList.toggle('hidden_div')
+    
+})
+
 
 // pour la nav bar 
 
@@ -24,6 +48,23 @@ let navbar = document.querySelector('#header')
 
     })
 
+// pareil en responsive
+
+
+let menuburger = document.querySelector('#scroll_burguer')
+
+    window.addEventListener('scroll', function () {
+
+        if ((this.window.scrollY) > 222) {
+            menuburger.classList.add("header_scroll_burguer")
+        } else if ((this.window.scrollY) < 222) {
+            menuburger.classList.remove("header_scroll_burguer")
+        }
+
+    })
+
+
+
 // test pour le mail 
 
 
@@ -35,9 +76,6 @@ window.onload = function() {
     
             document.getElementById('contact-form').addEventListener('submit', function(event) {
                 event.preventDefault();
-
-
-                        console.log('ppl')
                 
                 // these IDs from the previous steps
                 emailjs.sendForm('service_zney9zb', 'template_3fqpmsc', this)
@@ -49,6 +87,8 @@ window.onload = function() {
             });
         }
 
+
+// animation du skate de la nav bar 
 
 gsap.registerPlugin(ScrollTrigger);
 
